10a27efb164ac8c39ccfad3d20f2134ea0cdce58
lupuuss <damian.baczynski@gmail.com>
2023-06-20 01:20:34 +0200

    Add more targets

--- build-mokkery/src/main/kotlin/mokkery-multiplatform.gradle.kts
+++ build-mokkery/src/main/kotlin/mokkery-multiplatform.gradle.kts
@@ -7,7 +7,20 @@ kotlin {
     explicitApi()
     jvm()
     js(IR) {}
+
+    ios()
+    iosSimulatorArm64()
+
+    tvos()
+    tvosSimulatorArm64()
+
+    watchos()
+
+    macosArm64()
+    macosX64()
+
     mingwX64()
+    linuxX64()
 
     sourceSets {
         all {
@@ -16,12 +29,31 @@ kotlin {
         }
 
         val commonMain by getting
+
+        val darwinMain by creating
+
+        val iosMain by getting { dependsOn(darwinMain) }
+        val iosSimulatorArm64Main by getting { dependsOn(iosMain) }
+
+        val watchosMain by getting { dependsOn(darwinMain) }
+
+        val tvosMain by getting { dependsOn(darwinMain) }
+        val tvosSimulatorArm64Main by getting { dependsOn(darwinMain) }
+
+        val macosX64Main by getting { dependsOn(darwinMain) }
+        val macosArm64Main by getting { dependsOn(darwinMain) }
+
         val jvmMain by getting
+
         val mingwX64Main by getting
+        val linuxX64Main by getting
+
         val blockingMain by creating {
             dependsOn(commonMain)
             jvmMain.dependsOn(this)
             mingwX64Main.dependsOn(this)
+            linuxX64Main.dependsOn(this)
+            darwinMain.dependsOn(this)
         }
     }
 }
