a1c7f8493d8da26b030526410fc8d78350470c62
lupuuss <damian.baczynski@gmail.com>
2023-07-12 01:18:50 +0200

    Fix mokkery-plugin publishing

--- mokkery-plugin/build.gradle.kts
+++ mokkery-plugin/build.gradle.kts
@@ -1,7 +1,14 @@
+import MokkeryUrls.GitConnection
+import MokkeryUrls.GitDevConnection
+import MokkeryUrls.GitHttp
+import MokkeryUrls.Website
+
 plugins {
-    id("mokkery-publish")
+    id("org.jetbrains.dokka")
     kotlin("jvm")
     kotlin("kapt")
+    `maven-publish`
+    signing
 }
 
 kotlin.sourceSets.all {
@@ -22,14 +29,60 @@ dependencies {
     implementation(project(":mokkery-core"))
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group as String
-            artifactId = project.name
-            version = project.version as String
+loadLocalProperties()
 
-            from(components["java"])
+java {
+    withSourcesJar()
+}
+
+val dokkaJar by tasks.registering( Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    archiveClassifier.set("javadoc")
+    from(tasks.getByName("dokkaGfm"))
+}
+
+publishing {
+
+    repositories {
+        maven {
+            name = "sonatype"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = extraString("ossrhUsername")
+                password = extraString("ossrhPassword")
+            }
+        }
+    }
+    publications.create<MavenPublication>("maven") {
+        groupId = project.group as String
+        artifactId = project.name
+        version = project.version as String
+        artifact(dokkaJar)
+        from(components["java"])
+        pom {
+            name.set(project.name)
+            description.set(
+                "Kotlin compiler plugin for Mokkery - mocking library for Kotlin Multiplatform, easy to use, boilerplate-free and compiler plugin driven."
+            )
+            url.set(Website)
+            licenses {
+                license {
+                    name.set("The Apache Software License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    distribution.set("repo")
+                }
+            }
+            developers {
+                developer {
+                    id.set("lupuuss")
+                    name.set("lupuuss")
+                }
+            }
+            scm {
+                url.set(GitHttp)
+                connection.set(GitConnection)
+                developerConnection.set(GitDevConnection)
+            }
         }
     }
 }
