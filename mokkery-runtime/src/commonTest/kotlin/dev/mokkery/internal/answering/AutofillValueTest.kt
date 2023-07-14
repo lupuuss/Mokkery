920fd6bdd9ee48c0e7b7c0d415e8e980ed188486
lupuuss <damian.baczynski@gmail.com>
2023-07-14 23:02:19 +0200

    Add AutofillValueTest

---
+++ mokkery-runtime/src/commonTest/kotlin/dev/mokkery/internal/answering/AutofillValueTest.kt
@@ -0,0 +1,31 @@
+package dev.mokkery.internal.answering
+
+import dev.mokkery.internal.DefaultNothingException
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertNull
+
+class AutofillValueTest {
+
+    @Test
+    fun testCallReturnsFromAutofillMapping() {
+        autoFillMapping.forEach { (type, value) ->
+            assertEquals(value, autofillValue(type))
+        }
+    }
+
+    @Test
+    fun testCallThrowsNothingOnNothingClass() {
+        assertFailsWith<DefaultNothingException> {
+            autofillValue(Nothing::class)
+        }
+    }
+
+
+    @Test
+    fun testCallReturnsNullOnComplexType() {
+        assertNull(autofillValue((List::class)))
+    }
+
+}
