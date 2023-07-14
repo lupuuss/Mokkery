a54a264d56eaec6102a688b19b9a6b4691e37721
lupuuss <damian.baczynski@gmail.com>
2023-07-14 22:44:35 +0200

    Add doc to `isIn` and `isNotIn`

--- mokkery-runtime/src/commonMain/kotlin/dev/mokkery/matcher/collections/CollectionArgMatchersApi.kt
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/matcher/collections/CollectionArgMatchersApi.kt
@@ -3,19 +3,31 @@ package dev.mokkery.matcher.collections
 import dev.mokkery.matcher.ArgMatchersScope
 import dev.mokkery.matcher.matches
 
+/**
+ * Matches argument that is present in [values].
+ */
 public inline fun <reified T> ArgMatchersScope.isIn(
     vararg values: T
 ): T = matches(CollectionArgMatchers.ValueInIterable(values.toList()))
 
+/**
+ * Matches argument that is present in [values].
+ */
 public inline fun <reified T> ArgMatchersScope.isIn(
     values: Iterable<T>
 ): T = matches(CollectionArgMatchers.ValueInIterable(values))
 
 
+/**
+ * Matches argument that is not present in [values].
+ */
 public inline fun <reified T> ArgMatchersScope.isNotIn(
     vararg values: T
 ): T = matches(CollectionArgMatchers.ValueNotInIterable(values.toList()))
 
+/**
+ * Matches argument that is not present in [values].
+ */
 public inline fun <reified T> ArgMatchersScope.isNotIn(
     values: Iterable<T>
 ): T = matches(CollectionArgMatchers.ValueNotInIterable(values))
