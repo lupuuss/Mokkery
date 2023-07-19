8b04e91ea64973a344fef870af53b3cdc33b1f62
lupuuss <damian.baczynski@gmail.com>
2023-07-19 22:23:34 +0200

    Add dynamic MokkeryInterceptorScope implementations

--- mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/MokkeryInterceptorScope.kt
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/MokkeryInterceptorScope.kt
@@ -1,5 +1,7 @@
 package dev.mokkery.internal
 
+import dev.mokkery.MockMode
+
 internal interface MokkeryInterceptorScope {
 
     val id: String
@@ -16,4 +18,29 @@ internal interface MokkerySpyScope : MokkeryInterceptorScope {
 internal interface MokkeryMockScope : MokkerySpyScope {
 
     override val interceptor: MokkeryMock
+}
+
+
+internal fun MokkerySpyScope(typeName: String): MokkerySpyScope {
+    return DynamicMokkerySpyScope(typeName)
+}
+
+internal fun MokkeryMockScope(mode: MockMode, typeName: String): MokkeryMockScope {
+    return DynamicMokkeryMockScope(mode, typeName)
+}
+
+private class DynamicMokkeryMockScope(
+    mode: MockMode,
+    typeName: String,
+) : MokkeryMockScope {
+    override val interceptor = MokkeryMock(mode)
+
+    override val id = "$typeName@${hashCode().toString(33)}"
+}
+
+private class DynamicMokkerySpyScope(typeName: String) : MokkerySpyScope {
+
+    override val interceptor = MokkerySpy()
+
+    override val id = "$typeName@${hashCode().toString(33)}"
 }
