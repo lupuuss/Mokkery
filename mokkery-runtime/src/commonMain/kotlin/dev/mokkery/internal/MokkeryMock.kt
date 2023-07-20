d453bcb7527e24a55d173d2cbb5c5f3829da4a45
lupuuss <damian.baczynski@gmail.com>
2023-07-21 01:27:41 +0200

    Add mock mode to MokkeryMock api

--- mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/MokkeryMock.kt
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/MokkeryMock.kt
@@ -6,17 +6,20 @@ import dev.mokkery.internal.templating.TemplatingInterceptor
 import dev.mokkery.internal.tracing.CallTracingInterceptor
 
 internal interface MokkeryMock : MokkerySpy {
+    val mode: MockMode
     val answering: AnsweringInterceptor
 }
 
 @Suppress("unused")
 internal fun MokkeryMock(mockMode: MockMode): MokkeryMock = MokkeryMockImpl(
+    mockMode,
     TemplatingInterceptor(),
     CallTracingInterceptor(Counter.calls),
     AnsweringInterceptor(mockMode)
 )
 
 private class MokkeryMockImpl(
+    override val mode: MockMode,
     override val templating: TemplatingInterceptor,
     override val callTracing: CallTracingInterceptor,
     override val answering: AnsweringInterceptor,
