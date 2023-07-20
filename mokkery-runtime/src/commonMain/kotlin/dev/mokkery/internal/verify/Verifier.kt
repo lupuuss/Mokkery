e538e0ea0962a4d679cab3b14e76b95dd7d4346d
lupuuss <damian.baczynski@gmail.com>
2023-07-21 01:24:27 +0200

    Improve mockStateMessage

--- mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/verify/Verifier.kt
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/verify/Verifier.kt
@@ -16,5 +16,9 @@ internal fun failAssertion(
 
 private fun mockStateMessage(callTraces: List<CallTrace>, callTemplates: List<CallTemplate>): String {
     return "Expected patterns: \n\t${callTemplates.joinToString(separator = "\n\t")}\n" +
-            "Actual calls: \n\t${callTraces.joinToString(separator = "\n\t")}\n"
+            if (callTraces.isEmpty()) {
+                "Actual calls: \n\t Not registered!"
+            } else {
+                "Actual calls: \n\t${callTraces.joinToString(separator = "\n\t")}\n"
+            }
 }
