a61d3cc9ba4c476999554d51a9179bddbddc9feb
lupuuss <damian.baczynski@gmail.com>
2023-07-21 19:00:03 +0200

    Add debug functions

---
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/debug/Debug.kt
@@ -0,0 +1,73 @@
+package dev.mokkery.debug
+
+import dev.mokkery.answering.Answer
+import dev.mokkery.internal.MokkeryMock
+import dev.mokkery.internal.MokkeryMockScope
+import dev.mokkery.internal.MokkerySpy
+import dev.mokkery.internal.MokkerySpyScope
+import dev.mokkery.internal.dynamic.MokkeryScopeLookup
+
+/**
+ * Returns json-like structure of [obj] details (tracked calls, configured answers etc.).
+ */
+public fun mokkeryDebugString(obj: Any): String {
+    return when (val scope = MokkeryScopeLookup.current.resolve(obj)) {
+        is MokkeryMockScope -> mokkeryDebugMock(scope, scope.interceptor)
+        is MokkerySpyScope -> mokkeryDebugSpy(scope, scope.interceptor)
+        else -> "Not a mock/spy -> $obj"
+    }
+}
+
+/**
+ * Prints [mokkeryDebugString] result for [obj].
+ */
+public fun printMokkeryDebug(obj: Any) {
+    println(mokkeryDebugString(obj))
+}
+
+private fun mokkeryDebugMock(scope: MokkeryMockScope, mock: MokkeryMock): String {
+    return buildHierarchicalString {
+        section("mock") {
+            addValue("id", scope.toString())
+            addValue("mode", mock.mode.name)
+            section("answers") {
+                if (mock.answering.answers.isEmpty()) {
+                    addItem("")
+                } else {
+                    mock.answering.answers.forEach { (template, answer) ->
+                        addItem("${template.toStringNoReceiver()} ${answer.debug()}")
+                    }
+                }
+            }
+            callsSection(mock)
+        }
+    }
+}
+
+private fun mokkeryDebugSpy(scope: MokkerySpyScope, spy: MokkerySpy): String {
+    return buildHierarchicalString {
+        section("spy") {
+            addValue("id", scope.toString())
+            callsSection(spy)
+        }
+    }
+}
+
+private fun HierarchicalStringBuilder.callsSection(spy: MokkerySpy) {
+    section("calls") {
+        val calls = spy.callTracing.all
+        if (calls.isEmpty()) {
+            addItem("")
+            return@section
+        }
+        calls.forEach { addItem(it.toStringNoReceiver()) }
+    }
+}
+
+private fun Answer<*>.debug(): String = when (this) {
+    is Answer.Const<*> -> "returns $value"
+    is Answer.Throws -> "throws $throwable"
+    is Answer.Block, is Answer.BlockSuspend<*> -> "calls { ... }"
+    is Answer.Sequential -> "sequentially { ... }"
+    else -> "answers $this"
+}
