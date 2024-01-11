59f50ecb5fa1e1ee526ae28aa13a33c71c7ac83b
lupuuss <damian.baczynski@gmail.com>
2024-01-11 21:20:37 +0100

    Bump kotlin coroutines to 1.8.0-RC2

--- test-mokkery/build.gradle.kts
+++ test-mokkery/build.gradle.kts
@@ -56,7 +56,7 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0-RC2")
             }
         }
     }
