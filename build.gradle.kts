f0d7a7cf2559e9dc9680f4f6be42f3922b4faff0
lupuuss <damian.baczynski@gmail.com>
2024-01-17 19:40:52 +0100

    Bump version to 1.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ buildscript {
 }
 
 val kotlinVersion = libs.versions.kotlin.get()
-val mokkeryVersion = "1.3.1"
+val mokkeryVersion = "1.3.2"
 rootProject.ext["kotlinVersion"] = kotlinVersion
 rootProject.version = "${kotlinVersion}-${mokkeryVersion}"
 rootProject.group = "dev.mokkery"
