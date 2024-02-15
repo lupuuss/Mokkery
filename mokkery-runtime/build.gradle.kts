a649d7a7168481bce6c1ef7563192392c59d848d
lupuuss <damian.baczynski@gmail.com>
2024-02-15 20:30:43 +0100

    Add wasm for nodejs workaround to mokkery-runtime

--- mokkery-runtime/build.gradle.kts
+++ mokkery-runtime/build.gradle.kts
@@ -9,3 +9,12 @@ dependencies {
     commonTestImplementation(kotlin("test"))
     commonTestImplementation(libs.kotlinx.coroutines.test)
 }
+
+rootProject.extensions.configure(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension::class) {
+    nodeVersion = "21.0.0-v8-canary202309167e82ab1fa2"
+    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
+}
+
+rootProject.tasks.withType(org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask::class) {
+    args.add("--ignore-engines")
+}
