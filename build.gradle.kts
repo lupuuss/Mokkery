68a3cafd47cd5a49d63bfd9d8697bfce53b0d9af
lupuuss <damian.baczynski@gmail.com>
2024-02-19 21:23:02 +0100

    Add `Beta1` to mokkery version

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ buildscript {
 }
 
 val kotlinVersion = libs.versions.kotlin.get()
-val mokkeryVersion = "2.0.0"
+val mokkeryVersion = "2.0.0-Beta1"
 rootProject.ext["kotlinVersion"] = kotlinVersion
 rootProject.version = mokkeryVersion
 rootProject.group = "dev.mokkery"
