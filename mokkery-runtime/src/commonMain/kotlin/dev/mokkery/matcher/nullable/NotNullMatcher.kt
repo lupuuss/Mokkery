268d4a7c2394bb5449f42923d501e7a14d77e2fd
lupuuss <damian.baczynski@gmail.com>
2024-02-29 22:20:57 +0100

    Add notNull composite matcher

---
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/matcher/nullable/NotNullMatcher.kt
@@ -0,0 +1,40 @@
+package dev.mokkery.matcher.nullable
+
+import dev.mokkery.annotations.DelicateMokkeryApi
+import dev.mokkery.internal.MissingMatchersForComposite
+import dev.mokkery.matcher.ArgMatcher
+import dev.mokkery.matcher.ArgMatchersScope
+import dev.mokkery.matcher.any
+import dev.mokkery.matcher.capture.propagateCapture
+import dev.mokkery.matcher.matches
+
+/**
+ * Matches an argument that is not null and matches [matcher].
+ */
+@Suppress("UNUSED_PARAMETER")
+public inline fun <reified T : Any> ArgMatchersScope.notNull(matcher: T = any()): T? = matches(NotNullMatcher())
+
+/**
+ * Matches an argument that is not null and matches [matcher].
+ */
+@DelicateMokkeryApi
+public data class NotNullMatcher<T>(val matcher: ArgMatcher<T & Any>? = null) : ArgMatcher.Composite<T> {
+
+    override fun matches(arg: T): Boolean = arg?.let(matcher!!::matches) ?: false
+
+    override fun compose(matcher: ArgMatcher<T>): ArgMatcher.Composite<T> = copy(matcher = matcher)
+
+    override fun isFilled(): Boolean = matcher != null
+
+    override fun assertFilled() {
+        if (matcher == null) {
+            throw MissingMatchersForComposite("notNull", 1, listOfNotNull(matcher))
+        }
+    }
+
+    override fun capture(value: T) {
+        if (value != null) {
+            listOfNotNull(matcher).propagateCapture(value)
+        }
+    }
+}
---
+++ mokkery-runtime/src/commonTest/kotlin/dev/mokkery/matcher/nullable/NotNullMatcherTest.kt
@@ -0,0 +1,74 @@
+package dev.mokkery.matcher.nullable
+
+import dev.mokkery.internal.MissingMatchersForComposite
+import dev.mokkery.internal.unsafeCast
+import dev.mokkery.matcher.ArgMatcher
+import dev.mokkery.matcher.capture.CaptureMatcher
+import dev.mokkery.matcher.capture.asCapture
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class NotNullMatcherTest {
+
+    private val matcher = NotNullMatcher<Int?>(null)
+
+    @Test
+    fun testValidationFailsWhenMissingMatcher() {
+        assertFailsWith<MissingMatchersForComposite> {
+            matcher.assertFilled()
+        }
+    }
+
+    @Test
+    fun testValidationPassesWhenProperlyFilled() {
+        matcher
+            .compose(ArgMatcher.Equals(1))
+            .assertFilled()
+    }
+
+    @Test
+    fun testIsFilledReturnsFalseWhenMissingMatcher() {
+        assertFalse(matcher.isFilled())
+    }
+
+    @Test
+    fun testIsFilledReturnsTrueWhenFilled() {
+        assertTrue(matcher.compose(ArgMatcher.Equals(2)).isFilled())
+    }
+
+    @Test
+    fun testComposedMatcherMatchesWhenArgumentIsNotNullAndMatchingMatcher() {
+        val matcher = matcher.compose(ArgMatcher.Equals(1))
+        assertTrue(matcher.matches(1))
+    }
+
+    @Test
+    fun testComposedMatcherDoesNotMatchWhenArgumentIsNull() {
+        val matcher = matcher.compose(ArgMatcher.Equals(1))
+        assertFalse(matcher.matches(null))
+    }
+
+    @Test
+    fun testComposedMatcherDoesNotMatchWhenArgumentIsNotNullButMatcherNotSatisfied() {
+        val matcher = matcher.compose(ArgMatcher.Equals(1))
+        assertFalse(matcher.matches(2))
+    }
+
+    @Test
+    fun testPropagatesCaptureOnlyWithNotNullArgument() {
+        val list = mutableListOf<Int>()
+        val matcher = CaptureMatcher(list.asCapture(), ArgMatcher.Any)
+        this.matcher
+            .compose(matcher.unsafeCast())
+            .apply {
+                capture(1)
+                capture(null)
+                capture(2)
+                capture(null)
+            }
+        assertEquals(listOf(1, 2), list)
+    }
+}
