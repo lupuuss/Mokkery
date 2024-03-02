d1de4ae13906d7fc318a119a6d2fad20ca88fb1d
lupuuss <damian.baczynski@gmail.com>
2024-03-02 21:00:55 +0100

    Simplify wasm wasi test adjustment for functional tests

--- test-mokkery/src/coroutinesTest/kotlin/dev/mokkery/test/RunTest.coroutines.kt
+++ test-mokkery/src/coroutinesTest/kotlin/dev/mokkery/test/RunTest.coroutines.kt
@@ -7,3 +7,10 @@ annotation class NoIgnore
 actual suspend fun delay(duration: Long) {
     kotlinx.coroutines.delay(duration)
 }
+
+@Suppress("ACTUAL_TYPE_ALIAS_NOT_TO_CLASS", "ACTUAL_WITHOUT_EXPECT")
+actual typealias TestResult = kotlinx.coroutines.test.TestResult
+
+actual fun runTest(block: suspend () -> Unit): TestResult = kotlinx.coroutines.test.runTest {
+    block()
+}
