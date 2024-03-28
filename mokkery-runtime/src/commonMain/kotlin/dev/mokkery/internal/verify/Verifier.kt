f7858c99f9cd76ee7e98db5413508cd159d3c319
lupuuss <damian.baczynski@gmail.com>
2024-03-29 00:28:06 +0100

    Removed failAssertion from Verifier.kt

--- mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/verify/Verifier.kt
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/verify/Verifier.kt
@@ -8,17 +8,3 @@ internal sealed interface Verifier {
     fun verify(callTraces: List<CallTrace>, callTemplates: List<CallTemplate>): List<CallTrace>
 }
 
-internal fun failAssertion(
-    callTraces: List<CallTrace>,
-    callTemplates: List<CallTemplate>,
-    message: () -> String
-): Nothing = throw AssertionError("${message()}\n" + mockStateMessage(callTraces, callTemplates))
-
-private fun mockStateMessage(callTraces: List<CallTrace>, callTemplates: List<CallTemplate>): String {
-    return "Expected patterns: \n\t${callTemplates.joinToString(separator = "\n\t")}\n" +
-            if (callTraces.isEmpty()) {
-                "Actual calls: \n\t Not registered!"
-            } else {
-                "Actual calls: \n\t${callTraces.joinToString(separator = "\n\t")}\n"
-            }
-}
