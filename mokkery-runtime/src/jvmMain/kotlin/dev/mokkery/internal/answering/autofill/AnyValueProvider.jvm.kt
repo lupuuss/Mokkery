1cccd9b995b2b7914751628904d3e5324037b30c
lupuuss <damian.baczynski@gmail.com>
2024-04-05 01:33:52 +0200

    Add value class instantiation in AnyValueProvider for JVM

--- mokkery-runtime/src/jvmMain/kotlin/dev/mokkery/internal/answering/autofill/AnyValueProvider.jvm.kt
+++ mokkery-runtime/src/jvmMain/kotlin/dev/mokkery/internal/answering/autofill/AnyValueProvider.jvm.kt
@@ -5,5 +5,20 @@ import dev.mokkery.answering.autofill.AutofillProvider.Value
 import kotlin.reflect.KClass
 
 internal actual object AnyValueProvider : AutofillProvider<Any?> {
-    actual override fun provide(type: KClass<*>): Value<Any?> = Value.Provided(null)
+
+    actual override fun provide(type: KClass<*>): Value<Any?> {
+        val jClass = type.java
+        return when {
+            jClass.isAnnotationPresent(JvmInline::class.java) -> instantiateInlineClassOf(jClass)
+            else -> null
+        }.asAutofillProvided()
+    }
+
+    private fun instantiateInlineClassOf(jClass: Class<*>): Any = jClass.declaredConstructors
+        .first()
+        .let {
+            val paramType = it.parameterTypes.first().kotlin
+            it.isAccessible = true
+            it.newInstance(buildInTypesMapping[paramType])
+        }
 }
