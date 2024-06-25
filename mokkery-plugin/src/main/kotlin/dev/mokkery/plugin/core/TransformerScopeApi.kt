b2a39416067c086ebd6090431b7df71fb8671f47
lupuuss <damian.baczynski@gmail.com>
2024-06-25 19:33:35 +0200

    Use new message collector util in a backward compatible way

--- mokkery-plugin/src/main/kotlin/dev/mokkery/plugin/core/TransformerScopeApi.kt
+++ mokkery-plugin/src/main/kotlin/dev/mokkery/plugin/core/TransformerScopeApi.kt
@@ -1,16 +1,14 @@
 package dev.mokkery.plugin.core
 
+import dev.mokkery.plugin.messageCollectorCompat
 import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
-import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
-import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.ir.builders.IrGeneratorContext
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.symbols.IrSymbol
 
-val TransformerScope.messageCollector get() = compilerConfig
-    .get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
+val TransformerScope.messageCollector get() = compilerConfig.messageCollectorCompat
 
 fun TransformerScope.getClass(resolver: ClassResolver): IrClass = classes.getOrPut(resolver) {
     resolver.resolve(pluginContext)
