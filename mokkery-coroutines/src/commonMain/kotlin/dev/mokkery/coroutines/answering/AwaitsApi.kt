0b62b85af33f02d275dc738887a0cca70a5eeabb
lupuuss <damian.baczynski@gmail.com>
2024-08-04 16:26:46 +0200

    Add `awaits(Deferred<T>)` and `awaits((SuspendCallDefinitionScope<T>) -> Deferred<T>)`

--- mokkery-coroutines/src/commonMain/kotlin/dev/mokkery/coroutines/answering/AwaitsApi.kt
+++ mokkery-coroutines/src/commonMain/kotlin/dev/mokkery/coroutines/answering/AwaitsApi.kt
@@ -1,11 +1,28 @@
 package dev.mokkery.coroutines.answering
 
 import dev.mokkery.answering.SuspendAnsweringScope
+import dev.mokkery.answering.SuspendCallDefinitionScope
 import dev.mokkery.coroutines.internal.answering.AwaitAnswer
+import dev.mokkery.coroutines.internal.answering.AwaitDeferred
+import kotlinx.coroutines.Deferred
 
 /**
  * Function call awaits for specified [awaitable].
  */
 public infix fun <T> SuspendAnsweringScope<T>.awaits(awaitable: Awaitable<T>) {
     answers(AwaitAnswer(awaitable))
+}
+
+/**
+ * Function call awaits for [deferred].
+ */
+public infix fun <T> SuspendAnsweringScope<T>.awaits(deferred: Deferred<T>) {
+    awaits(AwaitDeferred(description = deferred::toString, deferred = { deferred }))
+}
+
+/**
+ * Function call awaits on each call for a [Deferred] provided by [provider].
+ */
+public infix fun <T> SuspendAnsweringScope<T>.awaits(provider: (SuspendCallDefinitionScope<T>) -> Deferred<T>) {
+    awaits(AwaitDeferred(description = { "{...}" }, deferred = { SuspendCallDefinitionScope<T>(it).let(provider) }))
 }
