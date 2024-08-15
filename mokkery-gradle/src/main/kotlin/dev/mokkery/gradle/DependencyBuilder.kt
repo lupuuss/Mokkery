a87a9ebfd939ca8688f654bc099dbb2a12a757a0
lupuuss <damian.baczynski@gmail.com>
2024-08-15 22:20:18 +0200

    Add `mokkery(module: String, ...)` overload for KotlinDependencyHandler

--- mokkery-gradle/src/main/kotlin/dev/mokkery/gradle/DependencyBuilder.kt
+++ mokkery-gradle/src/main/kotlin/dev/mokkery/gradle/DependencyBuilder.kt
@@ -2,7 +2,16 @@ package dev.mokkery.gradle
 
 import dev.mokkery.MokkeryConfig
 import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 
 public fun DependencyHandler.mokkery(module: String, version: String? = null): String {
+    return mokkeryDependency(module, version)
+}
+
+public fun KotlinDependencyHandler.mokkery(module: String, version: String? = null): String {
+    return mokkeryDependency(module, version)
+}
+
+private fun mokkeryDependency(module: String, version: String?): String {
     return "${MokkeryConfig.GROUP}:mokkery-$module:${version ?: MokkeryConfig.VERSION}"
 }
