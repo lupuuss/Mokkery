f00222c2fbb6383581a8b0b412ad4d8d97463c9e
lupuuss <damian.baczynski@gmail.com>
2024-08-20 23:52:01 +0200

    Add autofillConstructor in CompilerPluginUtils.kt

--- mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/CompilerPluginUtils.kt
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/CompilerPluginUtils.kt
@@ -2,4 +2,15 @@
 
 package dev.mokkery.internal
 
+import dev.mokkery.answering.autofill.AutofillProvider
+import dev.mokkery.answering.autofill.provideValue
+import kotlin.reflect.KClass
+
 internal fun generateMockId(typeName: String) = MockUniqueReceiversGenerator.generate(typeName)
+
+internal fun <T> autofillConstructor(type: KClass<*>): T = autofillConstructorProvider
+    .provideValue(type)
+    .unsafeCast()
+
+private val autofillConstructorProvider = AutofillProvider
+    .forInternals
