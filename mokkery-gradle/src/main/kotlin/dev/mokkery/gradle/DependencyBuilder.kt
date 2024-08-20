f61b029111341719cf9886df617d1ae9dd552b0f
lupuuss <damian.baczynski@gmail.com>
2024-08-20 19:02:30 +0200

    Add documentation to Gradle dependency utils

--- mokkery-gradle/src/main/kotlin/dev/mokkery/gradle/DependencyBuilder.kt
+++ mokkery-gradle/src/main/kotlin/dev/mokkery/gradle/DependencyBuilder.kt
@@ -4,10 +4,18 @@ import dev.mokkery.MokkeryConfig
 import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 
+/**
+ * Builds the dependency notation for the named Mokkery [module] at the given [version].
+ * If [version] is not specified, it uses current Mokkery version ([MokkeryConfig.VERSION])
+*/
 public fun DependencyHandler.mokkery(module: String, version: String? = null): String {
     return mokkeryDependency(module, version)
 }
 
+/**
+ * Builds the dependency notation for the named Mokkery [module] at the given [version].
+ * If [version] is not specified, it uses current Mokkery version ([MokkeryConfig.VERSION])
+ */
 public fun KotlinDependencyHandler.mokkery(module: String, version: String? = null): String {
     return mokkeryDependency(module, version)
 }
