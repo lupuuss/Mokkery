8d03050ecd32558d09aa6277a37ee3f8a8fd85db
lupuuss <damian.baczynski@gmail.com>
2024-10-19 18:59:20 +0200

    Fix MokkeryCallsChecker.kt (with backward compatibility) due to toRegularClassSymbol being moved to different package

--- mokkery-plugin/src/main/kotlin/dev/mokkery/plugin/fir/compat/FirCompat.kt
+++ mokkery-plugin/src/main/kotlin/dev/mokkery/plugin/fir/compat/FirCompat.kt
@@ -1,7 +1,10 @@
 package dev.mokkery.plugin.fir.compat
 
 import org.jetbrains.kotlin.diagnostics.rendering.DiagnosticParameterRenderer
+import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirDiagnosticRenderers
+import org.jetbrains.kotlin.fir.resolve.toRegularClassSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
 import org.jetbrains.kotlin.fir.types.ConeKotlinType
 
 @Suppress("UNCHECKED_CAST")
@@ -14,4 +17,19 @@ fun FirDiagnosticRenderers.renderTypeCompat(): DiagnosticParameterRenderer<ConeK
         .first { it.name == "getRENDER_TYPE" }
         .invoke(this)
         .let { it as DiagnosticParameterRenderer<ConeKotlinType> }
+}
+
+fun ConeKotlinType.toRegularClassSymbolCompat(session: FirSession): FirRegularClassSymbol? = try {
+    toRegularClassSymbol(session)
+} catch (e: NoClassDefFoundError) {
+    toRegularClassSymbolMethod
+        .invoke(null, this, session)
+        ?.let { it as FirRegularClassSymbol }
+}
+
+private val toRegularClassSymbolMethod by lazy {
+    ClassLoader.getSystemClassLoader()
+        .loadClass("org.jetbrains.kotlin.fir.types.TypeUtilsKt")
+        .methods
+        .first { it.name == "toRegularClassSymbol" && it.parameters.size == 2 }
 }
