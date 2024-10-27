1e776c184c7532b346d7b9511d94250c7d401fd4
lupuuss <damian.baczynski@gmail.com>
2024-10-27 13:17:06 +0100

    Extract MokkeryCallScopeApi.kt

--- mokkery-runtime/src/commonMain/kotlin/dev/mokkery/interceptor/MokkeryCallInterceptor.kt
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/interceptor/MokkeryCallInterceptor.kt
@@ -1,16 +1,8 @@
 package dev.mokkery.interceptor
 
 import dev.mokkery.annotations.DelicateMokkeryApi
-import dev.mokkery.answering.FunctionScope
-import dev.mokkery.context.CallArgument
-import dev.mokkery.context.FunctionCall
 import dev.mokkery.context.MokkeryContext
-import dev.mokkery.context.call
-import dev.mokkery.internal.context.associatedFunctions
-import dev.mokkery.internal.context.self
 import dev.mokkery.internal.interceptor.MokkeryCallHooks
-import dev.mokkery.internal.interceptor.nextInterceptor
-import kotlin.reflect.KClass
 
 /**
  * It's invoked on each mocked function call.
@@ -49,54 +41,6 @@ public interface MokkeryBlockingCallScope : MokkeryCallScope
  * Provides a set of operations specific for [MokkeryCallInterceptor.intercept]
  */
 public interface MokkerySuspendCallScope : MokkeryCallScope
-
-/**
- * Equivalent of `this` in the scope of currently called function.
- */
-public val MokkeryCallScope.self: Any?
-    get() = context.self
-
-/**
- * Returns current call.
- */
-public val MokkeryCallScope.call: FunctionCall
-    get() = context.call
-
-/**
- * Returns a map of available super calls for currently called function.
- */
-public val MokkeryCallScope.supers: Map<KClass<*>, Function<Any?>>
-    get() = context.associatedFunctions.supers
-
-/**
- * Creates [FunctionScope] from this [MokkeryCallScope]
- */
-public fun MokkeryCallScope.toFunctionScope(): FunctionScope {
-    val call = call
-    val function = call.function
-    return FunctionScope(
-        returnType = function.returnType,
-        args = call.args.map(CallArgument::value),
-        self = self,
-        supers = supers
-    )
-}
-
-/**
- * Calls [MokkeryCallInterceptor.intercept] on the next interceptor in the pipeline.
- * Adds [context] to the next pipeline context.
- */
-public fun MokkeryBlockingCallScope.nextIntercept(context: MokkeryContext = MokkeryContext.Empty): Any? {
-    return this.context.nextInterceptor.intercept(withContext(context))
-}
-
-/**
- * Calls [MokkeryCallInterceptor.intercept] on the next interceptor in the pipeline.
- * Adds [context] to the next pipeline context.
- */
-public suspend fun MokkerySuspendCallScope.nextIntercept(context: MokkeryContext = MokkeryContext.Empty): Any? {
-    return this.context.nextInterceptor.intercept(withContext(context))
-}
 
 internal fun MokkeryBlockingCallScope(context: MokkeryContext = MokkeryContext.Empty): MokkeryBlockingCallScope {
 
