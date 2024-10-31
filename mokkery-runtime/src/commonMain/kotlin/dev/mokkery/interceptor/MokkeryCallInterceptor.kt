c6fc34cfe0f428abb8103b5499af482baa2062ea
lupuuss <damian.baczynski@gmail.com>
2024-10-31 12:26:52 +0100

    Update MokkeryCallInterceptor documentation

--- mokkery-runtime/src/commonMain/kotlin/dev/mokkery/interceptor/MokkeryCallInterceptor.kt
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/interceptor/MokkeryCallInterceptor.kt
@@ -7,25 +7,38 @@ import dev.mokkery.internal.interceptor.MokkeryCallHooks
 /**
  * It's invoked on each mocked function call.
  */
-@DelicateMokkeryApi
 public interface MokkeryCallInterceptor {
 
+    /**
+     * Invoked on each regular mock call. To continue processing, call [MokkeryBlockingCallScope.nextIntercept].
+     * The behavior following this call depends on the hook used.
+     *
+     * For information on available hooks and their effects, refer to [MokkeryCallInterceptor.Companion].
+     */
+    @DelicateMokkeryApi
     public fun intercept(scope: MokkeryBlockingCallScope): Any?
 
+    /**
+     * Invoked on each suspend mock call. To continue processing, call [MokkerySuspendCallScope.nextIntercept].
+     * The behavior following this call depends on the hook used.
+     *
+     * For information on available hooks and their effects, refer to [MokkeryCallInterceptor.Companion].
+     */
+    @DelicateMokkeryApi
     public suspend fun intercept(scope: MokkerySuspendCallScope): Any?
 
     public companion object {
 
         /**
          * Allows registering interceptors after a call is traced but before an answer is provided.
+         * [nextIntercept] returns value from defined answers of a fallback depending on a mock mode.
          */
         public val beforeAnswering: MokkeryHook<MokkeryCallInterceptor> = MokkeryCallHooks.beforeAnswering
     }
 }
 
 /**
- * Provides a set of operations available
- * in a [MokkeryCallInterceptor.intercept] and [MokkeryCallInterceptor.intercept].
+ * Provides a set of operations available in a [MokkeryCallInterceptor.intercept] for both regular and suspend functions.
  */
 public interface MokkeryCallScope {
 
@@ -33,12 +46,12 @@ public interface MokkeryCallScope {
 }
 
 /**
- * Provides a set of operations specific for [MokkeryCallInterceptor.intercept]
+ * Provides a set of operations available in a [MokkeryCallInterceptor.intercept] for regular functions only.
  */
 public interface MokkeryBlockingCallScope : MokkeryCallScope
 
 /**
- * Provides a set of operations specific for [MokkeryCallInterceptor.intercept]
+ * Provides a set of operations available in a [MokkeryCallInterceptor.intercept] for suspend functions only.
  */
 public interface MokkerySuspendCallScope : MokkeryCallScope
 
