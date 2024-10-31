e2842254cccf495dc57c9ef38fd044822f0c6603
lupuuss <damian.baczynski@gmail.com>
2024-10-31 12:28:05 +0100

    Add MokkeryCallLogger

---
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/debug/MokkeryCallLogger.kt
@@ -0,0 +1,34 @@
+package dev.mokkery.debug
+
+import dev.mokkery.interceptor.MokkeryCallListener
+import dev.mokkery.interceptor.MokkeryCallScope
+import dev.mokkery.interceptor.call
+import dev.mokkery.interceptor.self
+import dev.mokkery.internal.utils.callToString
+
+/**
+ * Logs each mock call with [loggingFunction]. By default, the [loggingFunction] is [println].
+ *
+ * Log line can be changed using [lineTransformer].
+ *
+ * To enable the logger, it has to be registered in a hook.
+ *
+ * Example:
+ *
+ * ```kotlin
+ * MokkeryCallInterceptor
+ *    .beforeAnswering
+ *    .register(MokkeryCallLogger())
+ * ```
+ */
+public class MokkeryCallLogger(
+    private val lineTransformer: (String) -> String = { it },
+    private val loggingFunction: (String) -> Unit = ::println,
+) : MokkeryCallListener {
+
+    override fun onIntercept(scope: MokkeryCallScope) {
+        callToString(scope.self.toString(), scope.call.function.name, scope.call.args)
+            .let(lineTransformer)
+            .let(loggingFunction)
+    }
+}
