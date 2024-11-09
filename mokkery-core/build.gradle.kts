897a6b26d1ab4e8ec9139cdc67d24664f73de5da
lupuuss <damian.baczynski@gmail.com>
2024-11-09 18:41:56 +0100

    Add Kotlin version warnings

--- mokkery-core/build.gradle.kts
+++ mokkery-core/build.gradle.kts
@@ -7,9 +7,13 @@ plugins {
 buildConfig {
     val pluginProject = project(":mokkery-plugin")
     packageName(rootProject.group.toString())
-    buildConfigField("String", "GROUP", "\"${rootProject.group}\"")
-    buildConfigField("String", "VERSION", "\"${rootProject.version}\"")
-    buildConfigField("String", "RUNTIME", "\"mokkery-runtime\"")
-    buildConfigField("String", "PLUGIN_ID", "\"${rootProject.ext["pluginId"]}\"")
-    buildConfigField("String", "PLUGIN_ARTIFACT_ID", "\"${pluginProject.name}\"")
+    buildConfigField("String", "GROUP", str(rootProject.group))
+    buildConfigField("String", "VERSION", str(rootProject.version))
+    buildConfigField("String", "RUNTIME", str("mokkery-runtime"))
+    buildConfigField("String", "PLUGIN_ID", str(rootProject.ext["pluginId"]))
+    buildConfigField("String", "PLUGIN_ARTIFACT_ID", str(pluginProject.name))
+    buildConfigField("String", "MINIMUM_KOTLIN_VERSION", str("2.0.0"))
+    buildConfigField("String", "COMPILED_KOTLIN_VERSION", str(libs.versions.kotlin.get()))
 }
+
+private fun str(value: Any?) = "\"$value\""
