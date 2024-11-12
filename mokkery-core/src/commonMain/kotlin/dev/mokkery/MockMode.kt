4766ffe284c566b2f1a654199d17a6b28e99a8df
lupuuss <damian.baczynski@gmail.com>
2024-11-12 19:18:39 +0100

    Update MockMode documentation

--- mokkery-core/src/commonMain/kotlin/dev/mokkery/MockMode.kt
+++ mokkery-core/src/commonMain/kotlin/dev/mokkery/MockMode.kt
@@ -11,12 +11,22 @@ public enum class MockMode {
     strict,
 
     /**
-     * Provides default *empty* value (e.g. 0 for numbers, "" for string, null for complex types)
+     * Provides default *empty* value (e.g. 0 for numbers, "" for string, null for complex types).
+     *
+     * For generic types, defaults match the specified type argument
+     * (e.g., `mock<List<String>>(autofill).get(0)` returns `""` instead of `null`).
+     * Mokkery achieves this by preserving type arguments at runtime.
+     *
+     * This mock mode can be enhanced by registering custom providers to `AutofillProvider.forMockMode`.
      */
     autofill,
 
     /**
      * Returns [Unit] for functions that return [Unit], otherwise fails.
+     *
+     * For generic types, the default return value will be `Unit` based on the specified type argument
+     * (e.g., `mock<() -> Unit>(autoUnit).invoke()` returns `Unit` instead of `null`).
+     * Mokkery achieves this by preserving type arguments at runtime.
      */
     autoUnit,
 
