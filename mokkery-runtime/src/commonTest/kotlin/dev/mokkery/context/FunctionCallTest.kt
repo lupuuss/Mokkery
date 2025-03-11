7f91382b5fd88b39a87673e12a16f8bfe9b813c3
lupuuss <damian.baczynski@gmail.com>
2025-03-11 23:45:15 +0100

    Add FunctionCall.argValues

--- mokkery-runtime/src/commonTest/kotlin/dev/mokkery/context/FunctionCallTest.kt
+++ mokkery-runtime/src/commonTest/kotlin/dev/mokkery/context/FunctionCallTest.kt
@@ -8,10 +8,15 @@ import kotlin.test.assertEquals
 class FunctionCallTest {
 
     private val functionCall = fakeFunctionCall(args = listOf(fakeCallArg(1), fakeCallArg("3")))
-    
+
     @Test
     fun testArgValueReturnsCastedArgumentValue() {
         assertEquals(1, functionCall.argValue(0))
         assertEquals("3", functionCall.argValue(1))
+    }
+
+    @Test
+    fun testArgValuesReturnsAllCallArgumentValues() {
+        assertEquals(listOf(1, "3"), functionCall.argValues)
     }
 }
