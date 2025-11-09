99cc3a35230572d29e7bd30b9b99a237e725607e
lupuuss <damian.baczynski@gmail.com>
2025-11-10 00:52:59 +0100

    Ensure that MokkeryInstanceId.id is compared first

--- mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/MokkeryInstanceId.kt
+++ mokkery-runtime/src/commonMain/kotlin/dev/mokkery/internal/MokkeryInstanceId.kt
@@ -1,6 +1,23 @@
 package dev.mokkery.internal
 
-internal data class MokkeryInstanceId(val typeName: String, val id: Long) {
+internal class MokkeryInstanceId(val typeName: String, val id: Long) {
 
     override fun toString(): String = "$typeName($id)"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+        other as MokkeryInstanceId
+        if (id != other.id) return false
+        if (typeName != other.typeName) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + typeName.hashCode()
+        return result
+    }
+
+    fun copy(typeName: String = this.typeName, id: Long = this.id) = MokkeryInstanceId(typeName, id)
 }
