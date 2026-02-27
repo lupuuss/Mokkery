0f7c127e3b07b2141993b8bd1550ae59d43228ec
lupuuss <damian.baczynski@gmail.com>
2026-02-27 18:59:25 +0100

    Remove potential memory leak from MokkeryOption

--- mokkery-core-tooling/src/main/kotlin/dev/mokkery/internal/options/MokkeryOption.kt
+++ mokkery-core-tooling/src/main/kotlin/dev/mokkery/internal/options/MokkeryOption.kt
@@ -10,17 +10,39 @@ public data class MokkeryOption<T>(
     public val allowMultipleOccurrences: Boolean,
     public val type: MokkeryOptionType<T>,
     public val defaultValue: T?,
-    public val extras: MutableMap<Any?, Any?> = mutableMapOf(),
 )
 
 @InternalMokkeryApi
-public fun interface MokkeryOptionProjection<out P> {
+public interface MokkeryOptionProjection<out P> {
 
-    public fun create(option: MokkeryOption<*>): P
+    public fun project(option: MokkeryOption<*>): P
+
+    @InternalMokkeryApi
+    public companion object {
+
+        public operator fun <T> invoke(
+            builder: (MokkeryOption<*>) -> T
+        ): MokkeryOptionProjection<T> = DefaultMokkeryOptionProjection(builder)
+
+        public fun <T> cached(
+            builder: (MokkeryOption<*>) -> T
+        ): MokkeryOptionProjection<T> = CachedMokkeryProjection(builder)
+    }
+}
+
+private class DefaultMokkeryOptionProjection<T>(
+    private val builder: (MokkeryOption<*>) -> T
+) : MokkeryOptionProjection<T> {
+    override fun project(option: MokkeryOption<*>): T = builder(option)
+}
+
+private class CachedMokkeryProjection<T>(
+    private val builder: (MokkeryOption<*>) -> T
+) : MokkeryOptionProjection<T> {
+    private val store = mutableMapOf<MokkeryOption<*>, T>()
+    override fun project(option: MokkeryOption<*>): T = store.getOrPut(option) { builder(option) }
 }
 
 @Suppress("UNCHECKED_CAST")
 @InternalMokkeryApi
-public fun <P> MokkeryOption<*>.get(projection: MokkeryOptionProjection<P>): P {
-    return extras.getOrPut(projection) { projection.create(this) } as P
-}
+public fun <P> MokkeryOption<*>.get(projection: MokkeryOptionProjection<P>): P = projection.project(this)
